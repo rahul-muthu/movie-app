3f8058498b0bd8c5de70c4cd0eba696e8abbfd19
Nathan Umesh <nathan.umesh43@gmail.com>
2024-01-12 13:19:55 +0100

    changed resultsPage parameters and other miniscule changes
    Added Label to include app title

--- OMDB gui.py
+++ OMDB gui.py
@@ -17,9 +17,8 @@ class app:
         titlelabel = Label(centerframe, text="OMDB", font=("Nexa", "20", "bold"))
         dropdownbox = OptionMenu(centerframe, searchtype, "Movie", "TV", "Celeb")
         searchbar = Entry(centerframe, width=100)
-
         #inbetweeners
-        self.root.bind("<Return>", lambda x: self.searchCheck(searchbar.get()))
+        self.root.bind("<Return>", lambda x: self.searchCheck(searchbar.get(), searchtype.get()))
         dropdownbox.config(indicatoron=0)
         #baking
         titlelabel.grid(row=0, column=0, columnspan=2)
@@ -27,13 +26,18 @@ class app:
         searchbar.grid(row=1,column=1)
         centerframe.place(relx=0.5, rely=0.5, anchor=CENTER)
 
-    def searchCheck(self, searchval):
+    def searchCheck(self, searchval, searchtype):
         if searchval != "":
-            self.resultsPage()
+            self.resultsPage(searchval, searchtype)
 
-    def resultsPage(self):
+    def resultsPage(self, searchval, searchtype):
         for i in self.root.winfo_children():
             i.destroy()
+        #making
+        titlelabel = Label(self.root, text="OMDB", font=("Nexa", "20", "bold"))
+        #inbetweeners
+        #baking
+        titlelabel.pack()
 
 
 
