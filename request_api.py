aff9a06678730e42a8a31b518e360ba155c69502
Rahul <rahul.muthu005@gmail.com>
2024-01-13 17:32:31 +0400

    made a function according to nathan needs
    added a function to return a list of dictionaries containing movie name, poster path and rating

--- request_api.py
+++ request_api.py
@@ -2,12 +2,9 @@ import requests
 import json
 import math
 
-print("## OMDB ##\n")
-searchRequest = input("Search Omdb: ")
-print("")
 
 def jsonMedia(Name, Year, Type, Plot, Page):
-    params = {"apikey": "8892b8ee", "s": searchRequest, "type": Type, "plot": Plot, "y": Year, "page": Page}
+    params = {"apikey": "8892b8ee", "s": Name, "type": Type, "plot": Plot, "y": Year, "page": Page}
     r = requests.get("https://www.omdbapi.com/", params)
     # Request object created of page 1 of all movies with "Batman"
 
@@ -21,28 +18,56 @@ def jsonMedia(Name, Year, Type, Plot, Page):
 def allMovies(Name):
     movies = []
     json_dict = jsonMedia(Name, None, "movie", None, None)
-    for i in range(math.ceil(int(json_dict['totalResults'])/10)):
-        movies += jsonMedia(Name, None, "movie", "full", str(i+1))['Search']
-    return movies
-    
+    if json_dict["Response"] == 'True':
+        for i in range(math.ceil(int(json_dict['totalResults'])/10)):
+            movies += jsonMedia(Name, None, "movie", "full", str(i+1))['Search']
+        return movies
+    else:
+        print(json_dict["Error"])
+        return []
 
 def movieList(Name):
     for j in allMovies(Name):
         print(j['Title'])
+    
+def returnMovies(Name):
+    movies = []
+
+    for i in allMovies(Name):
+        params2 = {"api_key" : "0d3ee9a2b929a754d750acc53410454b"}
+        t = requests.get("https://api.themoviedb.org/3/movie/" + i["imdbID"], params2)
+        t = t.json()
+
+        print(i["imdbID"])
+        if len(t) > 3:
+            movies.append(dict([("name",i["Title"]),("Poster",i["Poster"]),("Description",t["overview"]),("Rating",t["popularity"])]))
+    
+    
+    return movies
 
 
 def allShows(Name):
     shows = []
     json_dict = jsonMedia(Name, None, "series", None, None)
-    for i in range(math.ceil(int(json_dict['totalResults'])/10)):
-        shows += jsonMedia(Name, None, "series", None, str(i+1))['Search']
-    return shows
+    if json_dict["Response"] == True:
+        for i in range(math.ceil(int(json_dict['totalResults'])/10)):
+            shows += jsonMedia(Name, None, "series", None, str(i+1))['Search']
+        return shows
+    else:
+        print(json_dict["Error"])
+        return []
     
 
 def showList(Name):
     for j in allShows(Name):
         print(j['Title'])
 
+print(returnMovies("Jack Reacher"))
+
+
+
+
     
-showList(searchRequest)
+
+
 
